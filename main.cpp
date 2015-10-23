c35b3da307bf02bc0ac6d547cb5758d0268283dd
Maxim Dankovtsev <max.dankow@gmail.com>
2015-10-23 21:20:33 +0300

    Add CanBeSuffix table. Add messages, shows correctness of expression.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ struct ExpressionInfo
     ExpressionInfo operator * (const ExpressionInfo &other);
     ExpressionInfo kleeneStar();
     std::vector<std::vector<bool>> canBeSubWord;
+    std::vector<std::vector<bool>> canBeSubSuffix;
     std::string expression;
 };
 
@@ -25,7 +26,12 @@ std::ostream& operator << (std::ostream &output, const ExpressionInfo &info)
         {
             output << info.canBeSubWord[begin][end] << ' ';
         }
-        output << '\n';
+        output << " | ";
+        for (size_t end = 0; end < info.canBeSubSuffix.size(); ++end)
+        {
+            output << info.canBeSubSuffix[begin][end] << ' ';
+        }
+        output << "\n";
     }
     output << '\n';
     return  output;
@@ -40,41 +46,64 @@ bool isInAlphabet(char c)
 ExpressionInfo::ExpressionInfo(size_t size)
 {
     canBeSubWord.assign(size, std::vector<bool>(size, false));
+    canBeSubSuffix.assign(size, std::vector<bool>(size, false));
 }
 
-ExpressionInfo::ExpressionInfo(std::string word, char c) {
+ExpressionInfo::ExpressionInfo(std::string word, char c)
+{
     assert(isInAlphabet(c));
     canBeSubWord.assign(word.length(), std::vector<bool>(word.length(), false));
     for (size_t i = 0; i < word.length(); ++i)
     {
         canBeSubWord[i][i] = word[i] == c;
     }
+    canBeSubSuffix.assign(word.length(), std::vector<bool>(word.length(), false));
+    for (size_t i = 0; i < word.length(); ++i)
+    {
+        canBeSubSuffix[i][i] = word[i] == c;
+    }
 }
 
-ExpressionInfo ExpressionInfo::operator+(const ExpressionInfo &other) {
+ExpressionInfo ExpressionInfo::operator+(const ExpressionInfo &other)
+{
     ExpressionInfo sumInfo(canBeSubWord.size());
     for (size_t begin = 0; begin < this->canBeSubWord.size(); ++begin)
     {
         for (size_t end = begin; end < this->canBeSubWord.size(); ++end)
         {
             sumInfo.canBeSubWord[begin][end] = this->canBeSubWord[begin][end] || other.canBeSubWord[begin][end];
+            sumInfo.canBeSubSuffix[begin][end] = this->canBeSubSuffix[begin][end] || other.canBeSubSuffix[begin][end];
         }
     }
     return sumInfo;
 }
 ExpressionInfo ExpressionInfo::operator*(const ExpressionInfo &other) {
     ExpressionInfo multInfo(canBeSubWord.size());
-    std::cout << *this << "******";
-    std::cout << other;
+//    std::cout << *this << "******";
+//    std::cout << other;
     for (size_t begin = 0; begin < this->canBeSubWord.size(); ++begin)
     {
-        for (size_t end = begin + (begin == 0)? 0 : -1; end < this->canBeSubWord.size(); ++end)
+        for (size_t end = begin; end < this->canBeSubWord.size(); ++end)
         {
             if (this->canBeSubWord[begin][end] && end != this->canBeSubWord.size() - 1)
             {
                 for (size_t copy = end/* + 1*/; copy < other.canBeSubWord.size(); ++copy)
                 {
                     multInfo.canBeSubWord[begin][copy] = multInfo.canBeSubWord[begin][copy] || other.canBeSubWord[end + 1][copy];
+                }
+            }
+        }
+    }
+    for (size_t begin = 0; begin < this->canBeSubSuffix.size(); ++begin)
+    {
+        for (size_t end = begin; end < this->canBeSubSuffix.size(); ++end)
+        {
+            multInfo.canBeSubSuffix[begin][end] = multInfo.canBeSubSuffix[begin][end] || other.canBeSubSuffix[begin][end];
+            if (this->canBeSubSuffix[begin][end] && end != this->canBeSubSuffix.size() - 1)
+            {
+                for (size_t copy = end; copy < other.canBeSubSuffix.size(); ++copy)
+                {
+                    multInfo.canBeSubSuffix[begin][copy] = multInfo.canBeSubSuffix[begin][copy] || other.canBeSubWord[end + 1][copy];
                 }
             }
         }
@@ -172,6 +201,7 @@ bool runSolution(const std::string &regularExpression, const std::string &word)
         }
         std::cout << parsingStack.top();
     }
+    return parsingStack.size() == 1;
 }
 
 int main() {
@@ -181,6 +211,13 @@ int main() {
     readInput(inputFile, regularExpression, word);
     assert(checkWord(word));
     std::cout << regularExpression << "    " << word << "\n";
-    runSolution(regularExpression, word);
+    if (runSolution(regularExpression, word))
+    {
+        std::cout << "All right. Expression (" << regularExpression << ") is correct.\n";
+    }
+    else
+    {
+        std::cout << "ERROR occurred! Expression (" << regularExpression << ") is WRONG!\n";
+    }
     return 0;
 }
