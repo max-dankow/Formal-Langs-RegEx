abaffe476e4d5bddfeaff0a77f24fd41f8cf686f
Maxim Dankovtsev <max.dankow@gmail.com>
2015-10-23 19:44:43 +0300

    Add Kleene star. TODO: change indexing system.

--- main.cpp
+++ main.cpp
@@ -5,18 +5,19 @@
 #include <vector>
 #include <assert.h>
 
-struct SubExpressionInfo
+struct ExpressionInfo
 {
-    SubExpressionInfo(){}
-    SubExpressionInfo(size_t size);
-    SubExpressionInfo(std::string word, char c);
-    SubExpressionInfo operator + (const SubExpressionInfo &other);
-    SubExpressionInfo operator * (const SubExpressionInfo &other);
+    ExpressionInfo(){}
+    ExpressionInfo(size_t size);
+    ExpressionInfo(std::string word, char c);
+    ExpressionInfo operator + (const ExpressionInfo &other);
+    ExpressionInfo operator * (const ExpressionInfo &other);
+    ExpressionInfo kleeneStar();
     std::vector<std::vector<bool>> canBeSubWord;
     std::string expression;
 };
 
-std::ostream& operator << (std::ostream &output, const SubExpressionInfo &info)
+std::ostream& operator << (std::ostream &output, const ExpressionInfo &info)
 {
     for (size_t begin = 0; begin < info.canBeSubWord.size(); ++begin)
     {
@@ -36,12 +37,12 @@ bool isInAlphabet(char c)
     return c == 'a' || c == 'b' || c == 'c';
 }
 
-SubExpressionInfo::SubExpressionInfo(size_t size)
+ExpressionInfo::ExpressionInfo(size_t size)
 {
     canBeSubWord.assign(size, std::vector<bool>(size, false));
 }
 
-SubExpressionInfo::SubExpressionInfo(std::string word, char c) {
+ExpressionInfo::ExpressionInfo(std::string word, char c) {
     assert(isInAlphabet(c));
     canBeSubWord.assign(word.length(), std::vector<bool>(word.length(), false));
     for (size_t i = 0; i < word.length(); ++i)
@@ -50,27 +51,28 @@ SubExpressionInfo::SubExpressionInfo(std::string word, char c) {
     }
 }
 
-
-SubExpressionInfo SubExpressionInfo::operator+(const SubExpressionInfo &other) {
-    SubExpressionInfo sumInfo(canBeSubWord.size());
+ExpressionInfo ExpressionInfo::operator+(const ExpressionInfo &other) {
+    ExpressionInfo sumInfo(canBeSubWord.size());
     for (size_t begin = 0; begin < this->canBeSubWord.size(); ++begin)
     {
-        for (size_t end = 0; end < this->canBeSubWord.size(); ++end)
+        for (size_t end = begin; end < this->canBeSubWord.size(); ++end)
         {
             sumInfo.canBeSubWord[begin][end] = this->canBeSubWord[begin][end] || other.canBeSubWord[begin][end];
         }
     }
     return sumInfo;
 }
-SubExpressionInfo SubExpressionInfo::operator*(const SubExpressionInfo &other) {
-    SubExpressionInfo multInfo(canBeSubWord.size());
+ExpressionInfo ExpressionInfo::operator*(const ExpressionInfo &other) {
+    ExpressionInfo multInfo(canBeSubWord.size());
+    std::cout << *this << "******";
+    std::cout << other;
     for (size_t begin = 0; begin < this->canBeSubWord.size(); ++begin)
     {
-        for (size_t end = 0; end < this->canBeSubWord.size(); ++end)
+        for (size_t end = begin + (begin == 0)? 0 : -1; end < this->canBeSubWord.size(); ++end)
         {
             if (this->canBeSubWord[begin][end] && end != this->canBeSubWord.size() - 1)
             {
-                for (size_t copy = end; copy < other.canBeSubWord.size(); ++ copy)
+                for (size_t copy = end/* + 1*/; copy < other.canBeSubWord.size(); ++copy)
                 {
                     multInfo.canBeSubWord[begin][copy] = multInfo.canBeSubWord[begin][copy] || other.canBeSubWord[end + 1][copy];
                 }
@@ -80,6 +82,23 @@ SubExpressionInfo SubExpressionInfo::operator*(const SubExpressionInfo &other) {
     return multInfo;
 }
 
+ExpressionInfo ExpressionInfo::kleeneStar() {
+    ExpressionInfo sumInfo = *this;
+    ExpressionInfo power = *this;
+//    std::cout << power;
+    for (size_t i = 0; i < canBeSubWord.size(); ++i)
+    {
+        power = power * (*this);
+//        std::cout << power;
+        sumInfo = sumInfo + power;
+//        std::cout << sumInfo;
+    }
+    for (size_t i = 1; i < canBeSubWord.size(); ++i)
+    {
+        sumInfo.canBeSubWord[i][i - 1] = true;
+    }
+    return sumInfo;
+}
 
 bool checkWord(std::string word)
 {
@@ -100,48 +119,54 @@ void readInput(std::istream &input, std::string &regularExpression, std::string 
 
 bool runSolution(const std::string &regularExpression, const std::string &word)
 {
-    std::stack<SubExpressionInfo> parsingStack;
+    std::stack<ExpressionInfo> parsingStack;
     for (size_t i = 0; i < regularExpression.length(); ++i)
     {
         switch (regularExpression[i]) {
             // todo: Объединить как-то эти три одинаковых случая с целью DRY.
             case 'a':
-                parsingStack.push(SubExpressionInfo(word, 'a'));
+                parsingStack.push(ExpressionInfo(word, 'a'));
                 break;
             case 'b':
-                parsingStack.push(SubExpressionInfo(word, 'b'));
+                parsingStack.push(ExpressionInfo(word, 'b'));
                 break;
             case 'c':
-                parsingStack.push(SubExpressionInfo(word, 'c'));
+                parsingStack.push(ExpressionInfo(word, 'c'));
                 break;
             case '1':
-                parsingStack.push(SubExpressionInfo(word.length()));
+                parsingStack.push(ExpressionInfo(word.length()));
                 break;
             case '.': {
                 if (parsingStack.size() < 2) {
                     return false;
                 }
-                SubExpressionInfo first = parsingStack.top();
+                ExpressionInfo first = parsingStack.top();
                 parsingStack.pop();
-                SubExpressionInfo second = parsingStack.top();
+                ExpressionInfo second = parsingStack.top();
                 parsingStack.pop();
-                parsingStack.push(first + second);
+                parsingStack.push(second * first);
                 break;
             }
             case '+': {
-                if (parsingStack.size() < 2)
-                {
+                if (parsingStack.size() < 2) {
                     return false;
                 }
-                SubExpressionInfo first = parsingStack.top();
+                ExpressionInfo first = parsingStack.top();
                 parsingStack.pop();
-                SubExpressionInfo second = parsingStack.top();
+                ExpressionInfo second = parsingStack.top();
                 parsingStack.pop();
                 parsingStack.push(first + second);
                 break;
             }
-            case '*':
+            case '*': {
+                if (parsingStack.size() < 1) {
+                    return false;
+                }
+                ExpressionInfo first = parsingStack.top();
+                parsingStack.pop();
+                parsingStack.push(first.kleeneStar());
                 break;
+            }
             default:
                 assert(false);
         }
